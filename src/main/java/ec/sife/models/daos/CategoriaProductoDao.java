e2f4aa63de9dc0c4722933c14b520c6e2a59aa73
LUIS QUISHPI <luis.quishpi@outlook.com>
2015-10-11 00:04:45 +0200

    Sw creA CategoriaProductoDao

---
+++ src/main/java/ec/sife/models/daos/CategoriaProductoDao.java
@@ -0,0 +1,12 @@
+package ec.sife.models.daos;
+
+import ec.sife.models.entities.CategoriaProducto;
+
+public class CategoriaProductoDao extends GenericDao<CategoriaProducto, Integer> {
+
+    public CategoriaProductoDao() {
+        super(CategoriaProductoDao.class);
+    }
+
+
+}
