2214d3b15acb0a1f13e8f2110817c5234fd96e50
LUIS QUISHPI <luis.quishpi@outlook.com>
2015-10-11 00:08:29 +0200

    Update CategoriaProductoDao / Fix Error

--- src/main/java/ec/sife/models/daos/CategoriaProductoDao.java
+++ src/main/java/ec/sife/models/daos/CategoriaProductoDao.java
@@ -5,7 +5,7 @@ import ec.sife.models.entities.CategoriaProducto;
 public class CategoriaProductoDao extends GenericDao<CategoriaProducto, Integer> {
 
     public CategoriaProductoDao() {
-        super(CategoriaProductoDao.class);
+        super(CategoriaProducto.class);
     }
 
 
