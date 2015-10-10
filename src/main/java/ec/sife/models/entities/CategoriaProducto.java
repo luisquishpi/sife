bb8201c4b8ea14ee538df6bc74c6cbfd9ca93ed7
Celia Chacha <Celia@Celia-PC>
2015-10-10 16:53:26 -0500

    Creado entidad categoriaproducto

---
+++ src/main/java/ec/sife/models/entities/CategoriaProducto.java
@@ -0,0 +1,62 @@
+
+package ec.sife.models.entities;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoriaproducto")
+public class CategoriaProducto {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
+
+	@Column(name = "nombre", nullable = false, length = 100)
+	private String nombre;
+
+	@Column(name = "dependencia", nullable = false)
+	private CategoriaProducto dependencia;
+
+	@Column(name = "escategoria", nullable = false)
+	private Boolean escategoria = false;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public CategoriaProducto getDependencia() {
+		return dependencia;
+	}
+
+	public void setDependencia(CategoriaProducto dependencia) {
+		this.dependencia = dependencia;
+	}
+
+	public Boolean getEscategoria() {
+		return escategoria;
+	}
+
+	public void setEscategoria(Boolean escategoria) {
+		this.escategoria = escategoria;
+	}
+
+}
