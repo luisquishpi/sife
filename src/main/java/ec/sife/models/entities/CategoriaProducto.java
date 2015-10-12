9fa07cda5c5a4134bf39368686e1e2e6d5de1be6
LUIS QUISHPI <luis.quishpi@outlook.com>
2015-10-13 01:53:49 +0200

    Actualización CategoriaProducto, entidad y controlador

--- src/main/java/ec/sife/models/entities/CategoriaProducto.java
+++ src/main/java/ec/sife/models/entities/CategoriaProducto.java
@@ -9,65 +9,55 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
-
 @Entity
 @Table(name = "categoriaproducto")
 public class CategoriaProducto {
 
-	public CategoriaProducto() {
-		super();
-	}
+    public CategoriaProducto() {
+        super();
+    }
 
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	@Column(name = "id", unique = true, nullable = false)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
 
-	@Column(name = "nombre", nullable = false, length = 100)
-	private String nombre;
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
 
-	@Column(name = "dependencia", nullable = false)
-	private CategoriaProducto dependencia;
+    @Column(name = "dependencia", nullable = false)
+    private CategoriaProducto dependencia;
 
-	@Column(name = "contienecategoria", nullable = false)
-	private Boolean contienecategoria = true;
+    @Column(name = "contieneproductos", nullable = false)
+    private Boolean contieneProductos = true;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public CategoriaProducto getDependencia() {
-		return dependencia;
-	}
+    public CategoriaProducto getDependencia() {
+        return dependencia;
+    }
 
-	public void setDependencia(CategoriaProducto dependencia) {
-		this.dependencia = dependencia;
-	}
+    public void setDependencia(CategoriaProducto dependencia) {
+        this.dependencia = dependencia;
+    }
 
-	public Boolean getEscategoria() {
-		return contienecategoria;
-	}
-
-	public void setEscategoria(Boolean contienecategoria) {
-		this.contienecategoria = contienecategoria;
-	}
-	
-	  public String toString() {
-	        return "CategoriaProducto [id=" + id + ", nombre=" + nombre + ", dependencia="+dependencia
-	                + "escategoria="+contienecategoria+"]";
-	    }
+    public String toString() {
+        return "CategoriaProducto [id=" + id + ", nombre=" + nombre + ", dependencia=" + dependencia
+                + "escategoria=" + contieneProductos + "]";
+    }
 
 }
